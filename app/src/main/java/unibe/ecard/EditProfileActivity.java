8d1d41ececcf88bd38c31b910b0e6730dabf6a15
Mitra Mejia <mitra.mejia@gmail.com>
2016-04-11 11:13:46 -0400

    Add EditProfile Activity

---
+++ app/src/main/java/unibe/ecard/EditProfileActivity.java
@@ -0,0 +1,13 @@
+package unibe.ecard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_profile_activity);
+    }
+}
