b9949bfacbd022965e6bf6a915eed3a07d240e58
Mitra Mejia <mitra.mejia@gmail.com>
2016-04-13 00:57:08 -0400

    Implement send message method to write via nfc

--- app/src/main/java/unibe/ecard/ShareViaNfcActivity.java
+++ app/src/main/java/unibe/ecard/ShareViaNfcActivity.java
@@ -12,6 +12,7 @@ import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -35,31 +36,33 @@ public class ShareViaNfcActivity extends AppCompatActivity implements NfcAdapter
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.share_via_nfc_activity);
+        String message = "Mitra!!!!";
+        messagesToSendArray.add(message);
 
         myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
         //Check if NFC is available on device
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
-        if(mNfcAdapter != null) {
+        if (mNfcAdapter != null) {
             //This will refer back to createNdefMessage for what it will send
             mNfcAdapter.setNdefPushMessageCallback(this, this);
 
             //This will be called if the message is sent successfully
             mNfcAdapter.setOnNdefPushCompleteCallback(this, this);
-        }
-        else {
+        } else {
             Toast.makeText(this, "NFC not available on this device",
                     Toast.LENGTH_SHORT).show();
         }
 
-        try{
+        try {
+
             if (getIntent().getAction().equals(NfcAdapter.ACTION_NDEF_DISCOVERED)) {
                 handleNfcIntent(getIntent());
             }
-        }catch (NullPointerException npEx){
+        } catch (NullPointerException npEx) {
 
-            Toast.makeText(this, npEx.getMessage(),
+            Toast.makeText(this, npEx.getMessage() ,
                     Toast.LENGTH_SHORT).show();
         }
 
@@ -68,13 +71,15 @@ public class ShareViaNfcActivity extends AppCompatActivity implements NfcAdapter
     @Override
     public void onNdefPushComplete(NfcEvent event) {
         messagesToSendArray.clear();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     public NdefRecord[] createRecords() {
         NdefRecord[] records = new NdefRecord[messagesToSendArray.size() + 1];
         //To Create Messages Manually if API is less than
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
-            for (int i = 0; i < messagesToSendArray.size(); i++){
+            for (int i = 0; i < messagesToSendArray.size(); i++) {
                 byte[] payload = messagesToSendArray.get(i).
                         getBytes(Charset.forName("UTF-8"));
                 NdefRecord record = new NdefRecord(
@@ -88,11 +93,11 @@ public class ShareViaNfcActivity extends AppCompatActivity implements NfcAdapter
         }
         //Api is high enough that we can use createMime, which is preferred.
         else {
-            for (int i = 0; i < messagesToSendArray.size(); i++){
+            for (int i = 0; i < messagesToSendArray.size(); i++) {
                 byte[] payload = messagesToSendArray.get(i).
                         getBytes(Charset.forName("UTF-8"));
 
-                NdefRecord record = NdefRecord.createMime("text/plain",payload);
+                NdefRecord record = NdefRecord.createMime("text/plain", payload);
                 records[i] = record;
             }
         }
@@ -103,7 +108,7 @@ public class ShareViaNfcActivity extends AppCompatActivity implements NfcAdapter
     @Override
     public NdefMessage createNdefMessage(NfcEvent event) {
 
-        Toast.makeText(this, "Hizo Algo", Toast.LENGTH_LONG).show();
+//        Toast.makeText(this, "Hizo Algo", Toast.LENGTH_LONG).show();
 
 
         //This will be called when another NFC capable device is detected.
@@ -121,22 +126,24 @@ public class ShareViaNfcActivity extends AppCompatActivity implements NfcAdapter
             Parcelable[] receivedArray =
                     NfcIntent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
 
-            if(receivedArray != null) {
+            if (receivedArray != null) {
                 messagesReceivedArray.clear();
                 NdefMessage receivedMessage = (NdefMessage) receivedArray[0];
                 NdefRecord[] attachedRecords = receivedMessage.getRecords();
 
-                for (NdefRecord record:attachedRecords) {
+                for (NdefRecord record : attachedRecords) {
                     String string = new String(record.getPayload());
                     //Make sure we don't pass along our AAR (Android Applicatoin Record)
-                    if (string.equals(getPackageName())) { continue; }
+                    if (string.equals(getPackageName())) {
+                        continue;
+                    }
                     messagesReceivedArray.add(string);
+                    Log.d("NFC: sent ", string);
                 }
                 Toast.makeText(this, "Received " + messagesReceivedArray.size() +
                         " Messages", Toast.LENGTH_LONG).show();
 
-            }
-            else {
+            } else {
                 Toast.makeText(this, "Received Blank Parcel", Toast.LENGTH_LONG).show();
             }
         }
@@ -162,7 +169,7 @@ public class ShareViaNfcActivity extends AppCompatActivity implements NfcAdapter
     public void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
         savedInstanceState.putStringArrayList("messagesToSend", messagesToSendArray);
-        savedInstanceState.putStringArrayList("lastMessagesReceived",messagesReceivedArray);
+        savedInstanceState.putStringArrayList("lastMessagesReceived", messagesReceivedArray);
     }
 
     //Load our Array Lists of Messages for when the user navigates back
